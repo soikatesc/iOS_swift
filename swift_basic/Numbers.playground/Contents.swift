13cfb385afa49cb5180c92c29b7c88b152066c2a
Md Rahaman <soikatesc@gmail.com>
2017-07-03 23:03:01 -0500

    Numbers

---
+++ swift_basic/Numbers.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Numbers
+//Type inference
+var age = 30
+
+//Explicity declared type
+var weight: Int = 200
+
+var someNum: Double = 123456789
+var mileRun = 12.3
+
+var pi:Float = 3.14
+
+//Arithemetic Operators
+var area = 15 * 20
+var sum = 5 + 6
+var diff = 6-5
+var div = 12 / 3
+var div1 = 13 / 5
+var reminder = 13 % 5
+
+var result = "The result of 13/5 is \(div1) and reminder: \(reminder)"
+
+var
